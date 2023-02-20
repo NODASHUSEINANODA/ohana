848be65949d45ada000d4977a76c587d6ab102b5
tamashiro-syuta <syuta5825@gmail.com>
2023-02-21 01:01:25 +0900

    recoilを使ったサンプルのコンポーネントを定義

---
+++ src/recoil/Button/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { actions, selectors } from "./store";
+
+const RecoilButton = () => {
+  const seOnetNumber = actions.useAddOneNumber()
+  const setMinusOneNumber = actions.useMinusOneNumber()
+  const number = selectors.useNumber()
+  const square = selectors.useSquare()
+
+  return (
+    <>
+      <h1>number: {number}</h1>
+      <button
+        onClick={() => {
+          seOnetNumber()
+        }}
+      >+1</button>
+
+      <br />
+
+      <button
+        onClick={() => {
+          setMinusOneNumber()
+        }}
+      >-1</button>
+
+      <h1>{number} ** 2 = {square}</h1>
+    </>
+  )
+}
+
+export default RecoilButton
