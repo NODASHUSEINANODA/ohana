9c78df1ce464e2a9b3dfa05ff9802fde56fcdc8d
tamashiro-syuta <syuta5825@gmail.com>
2023-03-04 17:23:23 +0900

    feat : メンバー一覧をrecoilで状態管理

---
+++ src/home/components/Employees/store.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { atom, selector, useRecoilValue, useSetRecoilState } from "recoil";
+import Employees from "../../datasources/Employees";
+
+const key = 'home/employees'
+
+const state = atom({
+  key: `${key}/atom`,
+  default: {
+    loading: false,
+    employees: [],
+  }
+})
+
+const employees = selector({
+  key: `${key}/selector/employees`,
+  get: ({ get }) => {
+    const { employees } = get(state);
+
+    return employees ;
+  },
+});
+
+export const selectors = {
+  useEmployees: () => useRecoilValue(employees),
+}
+
+export const actions = {
+  useFetchEmployees: (() => {
+    const setState = useSetRecoilState(state)
+
+    return React.useCallback(async () => {
+      setState((prev) => ({ ...prev, loading: true }))
+      try {
+        const res = await Employees.get()
+        setState((prev) => ({ ...prev, employees: res.data }))
+      } catch (e) {
+        throw e
+      } finally {
+        setState((prev) => ({ ...prev, loading: false }))
+      }
+    }, [setState])
+  })
+}
