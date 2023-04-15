d3963275d7564e7e5753f610f07b526cdd7051e7
tamashiro-syuta <tamashiro_syuta@tamakinoMacBook-Pro.local>
2023-04-15 16:01:28 +0900

    fix : auth関連のファイルをcore配下に移動

--- src/lib/Client.js
+++ src/lib/Client.js
@@ -2,14 +2,14 @@ import applyCaseMiddleware from 'axios-case-converter'
 import axios from 'axios'
 
 const options = {
-    ignoreHeaders: true,
+  ignoreHeaders: true,
 }
 
 const client = applyCaseMiddleware(
-    axios.create({
-        baseURL: 'http://localhost:3000',
-    }),
-    options
+  axios.create({
+    baseURL: 'http://localhost:3000',
+  }),
+  options
 );
 
 export default client;
