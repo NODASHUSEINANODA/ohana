8fc8fb2787cdc22029b8dd90a644b59a102889ea
tamashiro-syuta <tamashiro_syuta@tamakinoMacBook-Pro.local>
2023-04-19 19:05:01 +0900

    fix : API側を作り終えたので、デモからAPI本体を叩くようにした

--- src/home/datasources/Employees.js
+++ src/home/datasources/Employees.js
@@ -3,9 +3,8 @@ import axios from "axios";
 export default class Employees {
   static async get(params) {
     try {
-      // TODO: API完成時に入れ替える
-      const URL = 'http://localhost:3002/employees'
-      return await axios.get( URL, { params: params });
+      const URL = 'http://localhost:3000/api/employees'
+      return await axios.get(URL, { params: params });
     } catch (error) {
       throw error
     }
