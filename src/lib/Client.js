23deb1185ce19246e8aae905b20b10ea6bec3eca
tamashiro-syuta <tamashiro_syuta@tamakinoMacBook-Pro.local>
2023-04-23 20:35:07 +0900

    fix : ほぼ全てのAPIとの通信にログインユーザーの情報が必要になると思うので、デフォルトでヘッダーを設定

--- src/lib/Client.js
+++ src/lib/Client.js
@@ -1,15 +1,16 @@
 import applyCaseMiddleware from 'axios-case-converter'
 import axios from 'axios'
+import Cookies from 'js-cookie';
 
-const options = {
-  ignoreHeaders: true,
-}
 
 const client = applyCaseMiddleware(
   axios.create({
     baseURL: 'http://localhost:3000',
   }),
-  options
 );
+
+client.defaults.headers.common['access-token'] = Cookies.get("_access_token")
+client.defaults.headers.common['client'] = Cookies.get("_client")
+client.defaults.headers.common['uid'] = Cookies.get("_uid")
 
 export default client;
